aa08af3b90ac9cb58fbf072ede2925dc3144174d
codedbyevan <tuannghia.tech@gmail.com>
2025-11-21 21:45:43 +0700

    Init caculator project

---
+++ src/Caculator.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+public class Caculator {
+  int boardHeight = 540;
+  int boardWidth = 360;
+
+  String[] buttonValues = {
+      "AC", "+/-", "%", "÷",
+      "7", "8", "9", "×",
+      "4", "5", "6", "-",
+      "1", "2", "3", "+",
+      "0", ".", "√", "="
+  };
+  String[] rightSymbols = { "÷", "×", "-", "+", "=" };
+  String[] topSymbols = { "AC", "+/-", "%" };
+
+  Color customLightGray = new Color(212, 212, 210);
+  Color customDarkGray = new Color(80, 80, 80);
+  Color customBlack = new Color(28, 28, 28);
+  Color customOrange = new Color(255, 149, 0);
+
+  JFrame frame = new JFrame("Caculator");
+  JLabel displayLabel = new JLabel();
+  JPanel displayPanel = new JPanel();
+  JPanel buttonsPanel = new JPanel();
+
+  Caculator() {
+    // frame.setVisible(true);
+    frame.setResizable(false);
+    frame.setLocationRelativeTo(null);
+    frame.setSize(boardWidth, boardHeight);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLayout(new BorderLayout());
+
+    displayLabel.setBackground(customBlack);
+    displayLabel.setForeground(Color.white);
+    displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+    displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+    displayLabel.setText("0");
+    displayLabel.setOpaque(true);
+
+    displayPanel.setLayout(new BorderLayout());
+    displayPanel.add(displayLabel);
+    frame.add(displayPanel, BorderLayout.NORTH);
+
+    buttonsPanel.setLayout(new GridLayout(5, 4));
+    buttonsPanel.setBackground(customBlack);
+    frame.add(buttonsPanel);
+
+    for (int i = 0; i < buttonValues.length; i++) {
+      JButton button = new JButton();
+      String buttonValue = buttonValues[i];
+      button.setFont(new Font("Arial", Font.PLAIN, 30));
+      button.setText(buttonValue);
+      button.setFocusable(false);
+      button.setBorder(new LineBorder(customBlack));
+      if (Arrays.asList(topSymbols).contains(buttonValue)) {
+        button.setBackground(customLightGray);
+        button.setForeground(customBlack);
+      } else if (Arrays.asList(rightSymbols).contains(buttonValue)) {
+        button.setBackground(customOrange);
+        button.setForeground(Color.white);
+      } else {
+        button.setBackground(customDarkGray);
+        button.setForeground(Color.white);
+      }
+      buttonsPanel.add(button);
+    }
+
+    frame.setVisible(true);
+  }
+}
